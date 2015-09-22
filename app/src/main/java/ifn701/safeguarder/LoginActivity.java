bf4eefdfdbd96e140066ee9bae835179bc078729
Jeevan <jeevan.basavanna@gmail.com>
2015-09-22 13:37:39 +1000

    Fixed small bug with login

--- app/src/main/java/ifn701/safeguarder/LoginActivity.java
+++ app/src/main/java/ifn701/safeguarder/LoginActivity.java
@@ -211,10 +211,10 @@ public class LoginActivity extends AppCompatActivity implements ILoginService{
             Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, MapsActivity.class);
             startActivity(intent);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 //            finish();
         }
         else
