9e5e7b34ad5f381e55bdea020ed983ea0321528f
Jeevan <jeevan.basavanna@gmail.com>
2015-10-23 22:06:52 +1000

    Fixed and Set location

--- backend/src/main/webapp/WEB-INF/jsp/viewmap.jsp
+++ backend/src/main/webapp/WEB-INF/jsp/viewmap.jsp
@@ -1,25 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta charset="utf-8">
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDO4n9AFniMpOQS3wkWH0pRUsmLxoqcxZ4&sensor=false">">
-    </script>
-    <script>
-      var map;
-      function initialize() {
-        var mapOptions ={
-          zoom: 8,
-          center: new google.maps.LatLng(-34.397,150.644),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-    </head>
+
+
 <%@include file="../templates/header.html"%>
 <link href="../stylesheets/main.css" rel="stylesheet">
 <body onload="initialize()">
@@ -33,7 +15,8 @@
     <div id="map-canvas" style="height:700px; width:1200px"></div>
     </div>
   <hr>
-<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+<script src="/js/map.js"></script>
 <%@include file="../templates/scripts.html"%>
 </body>
 <%@include file="../templates/footer.html"%>
--- backend/src/main/webapp/js/map.js
+++ backend/src/main/webapp/js/map.js
@@ -1,19 +1,8 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta charset="utf-8">
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDO4n9AFniMpOQS3wkWH0pRUsmLxoqcxZ4&sensor=false">">
-    </script>
-    <script>
       var map;
       function initialize() {
         var mapOptions ={
-          zoom: 8,
-          center: new google.maps.LatLng(-34.397,150.644),
+           zoom: 15,
+           center: new google.maps.LatLng(-27.477228, 153.028317),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         map = new google.maps.Map(document.getElementById("map-canvas"),
@@ -21,22 +10,14 @@
       }
       google.maps.event.addDomListener(window, 'load', initialize);
 
+      function loadScript() {
+       var script = document.createElement("script");
+       script.type = "text/javascript";
+       script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDO4n9AFniMpOQS3wkWH0pRUsmLxoqcxZ4&sensor=TRUE_OR_FALSE&callback=initialize";
+       document.body.appendChild(script);
+     }
 
+     window.onload = loadScript;
 
-//      function loadScript() {
-//       var script = document.createElement("script");
-//       script.type = "text/javascript";
-//       script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDO4n9AFniMpOQS3wkWH0pRUsmLxoqcxZ4&sensor=TRUE_OR_FALSE&callback=initialize";
-//       document.body.appendChild(script);
-//     }
-
-//     window.onload = loadScript;
-
-    </script>
-  </head>
-  <body onload="initialize()">
-    <div id="map-canvas" style="height:300px; width:500px"></div>
-  </body>
-</html>
 
 
